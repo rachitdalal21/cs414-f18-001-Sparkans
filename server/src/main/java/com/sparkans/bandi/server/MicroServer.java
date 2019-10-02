2e6f38fbe42faf62f9088e3c7018c856122b3cd9
derek chapman <derek@pop-os.localdomain>
2019-10-02 00:07:52 -0600

    added basic spark server built with maven

---
+++ server/src/main/java/com/sparkans/bandi/server/MicroServer.java
@@ -0,0 +1,96 @@
+package com.sparkans.bandi.server;
+
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
+
+/** A simple micro-server for the web.  Just what we need, nothing more.
+ *
+ */
+public class MicroServer {
+
+  private int    port;
+  private String name;
+  private String path = "/public";
+
+  /** Creates a micro-server to load static files and provide REST APIs.
+   *
+   * @param port
+   * @param name
+   */
+ public MicroServer(int port, String name) {
+    this.port = port;
+    this.name = name;
+
+    port(this.port);
+
+    // serve the static files: index.html and bundle.js
+    Spark.staticFileLocation(this.path);
+    get("/", (req, res) -> {
+      res.redirect("index.html");
+      return null;
+    });
+
+    // register all micro-services and the function that services them.
+    // start with HTTP GET
+    get("/about", this::about);
+    get("/echo", this::echo);
+    get("/hello/:name", this::hello);
+    //for client sending data, HTTP POST is used instead of a GET
+
+
+    System.out.println("\n\nServer running on port: " + this.port + "\n\n");
+  }
+
+  /** A REST API that describes the server.
+   *
+   * @param request
+   * @param response
+   * @return about string
+   */
+  private String about(Request request, Response response) {
+
+    response.type("text/html");
+
+    return "<html><head></head><body><h1>"+name+" Micro-server on port "+port+"</h1></body></html>";
+  }
+
+  /** A REST API that echos the client request.
+   *
+   * @param request
+   * @param response
+   * @return string echo response
+   */
+  private String echo(Request request, Response response) {
+
+    response.type("application/json");
+
+    return HTTP.echoRequest(request);
+  }
+
+  /** A REST API demonstrating the use of a parameter.
+   *
+   * @param request
+   * @param response
+   * @return hello string
+   */
+  private String hello(Request request, Response response) {
+
+    response.type("text/html");
+
+    return Greeting.html(request.params(":name"));
+  }
+
+ 
+  private String team(Request request, Response response) {
+
+    response.type("text/plain");
+
+    return name;
+  }
+}
---
+++ server/src/main/java/com/sparkans/bandi/server/MyServer.java
@@ -0,0 +1,48 @@
+package com.sparkans.bandi.server;
+
+/** The main class for the application.
+ *
+ */
+public class MyServer {
+
+  /** Main program starts a web microserver on the specified network port
+   ** @param args command line arguments optionally containing port and team name.
+   */
+  public static void main(String[] args) {
+
+    MicroServer server = new MicroServer(getPort(args), getName(args));
+
+  }
+
+  /** Obtain the port number from the command line arguments.  Defaults if none provided.
+   * @param args
+   * @return
+   */
+  private static int getPort(String[] args) {
+
+    if (args.length > 0) {
+      return Integer.parseInt(args[0]);
+    } else
+      return 31406; // team default
+
+  }
+
+  /** Obtain the name from the command line arguments.  Defaults if not specified.
+   * @param args
+   * @return a concatenation of the arguments after the port
+   */
+  private static String getName(String[] args) {
+
+    if (args.length > 1) {
+      String name = args[1];
+      for (int i = 2; i < args.length; i++) {
+        name = name + " " + args[i];
+      }
+      return name;
+    }
+    else {
+      return "Unknown";
+    }
+  }
+
+}
