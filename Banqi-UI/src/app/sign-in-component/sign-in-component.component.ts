9e7c13346a25e87af55c78800709dfba1599ae48
Rachit <rdalal@colostate.edu>
2019-10-25 20:54:41 -0600

    Sign In user made reactive and added validation - done

--- Banqi-UI/src/app/sign-in-component/sign-in-component.component.ts
+++ Banqi-UI/src/app/sign-in-component/sign-in-component.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Subscription} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-sign-in-component',
@@ -7,21 +9,44 @@ import {FormControl, Validators} from "@angular/forms";
   styleUrls: ['./sign-in-component.component.css']
 })
 export class SignInComponentComponent implements OnInit {
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [Validators.required]);
+  signInForm = new FormGroup({
+    email : new FormControl('', [Validators.required, Validators.email]),
+    password : new FormControl('', [Validators.required])
+  });
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
 
   ngOnInit() {
   }
 
   getErrorMessage(type: String) {
     if( type.length === 0 ){
-      return this.email.hasError('required') ? 'You must enter a value' :
-        this.email.hasError('email') ? 'Not a valid email' :
+      return this.signInForm.controls['email'].hasError('required') ? 'You must enter a value' :
+        this.signInForm.controls['email'].hasError('email') ? 'Not a valid email' :
           '';
     } else {
-      return this.password.hasError('required') ? 'You must enter a password' : '';
+      return this.signInForm.controls['password'].hasError('required') ? 'You must enter a password' : '';
     }
+  }
+
+  /*
+ * This method will be called when user click on Register user button
+ * */
+  signInUser ( value: FormGroup): Subscription {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    const userDetails = value;
+
+    return this.http.post<any>( "http://localhost:31406/register", userDetails, httpOptions)
+      .subscribe(( results ) => {
+        debugger;
+        // this.result = results;
+      }, (error) => {
+        debugger;
+      });
+
   }
 }
