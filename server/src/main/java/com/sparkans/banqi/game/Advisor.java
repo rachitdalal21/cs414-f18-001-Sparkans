a563bca9d8a36331b15031a9da0cf80c8294a466
djchap <derekchapman17@gmail.com>
2019-11-01 10:31:56 -0600

    forgot name for advisor, added it

--- server/src/main/java/com/sparkans/banqi/game/Advisor.java
+++ server/src/main/java/com/sparkans/banqi/game/Advisor.java
@@ -1,10 +1,15 @@
 package com.sparkans.banqi.game;
 
+import com.google.gson.annotations.Expose;
+
 import java.util.ArrayList;
 
 public class Advisor extends BanqiPiece {
 
 	ArrayList<String> legalMoves;
+
+	@Expose
+	private static final String name = "Advisor";
 
 	public Advisor(BanqiBoard board, Color color) {
 		super(board, color);
