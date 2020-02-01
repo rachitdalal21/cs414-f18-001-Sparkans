98163d0fe0bccecb56872c86acb138ed7a6204aa
rachitdalal21 <Rachit1@3>
2020-02-01 16:17:19 -0700

    Blur Image : Finding Average of the each element using neighbours of that element.

--- Banqi-UI/src/app/home/home.component.ts
+++ Banqi-UI/src/app/home/home.component.ts
@@ -10,6 +10,51 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+   // this.blurImg([[1,2,3,1], [4,5, 4,1], [1,4,6,1] ]);
   }
+  blurImg( img ) {
+    var newImg = [];
+    if( img && img.length > 0 ) {
+      for(var i = 0 ; i < img.length ; i++ ) {
+        newImg[i] = [];
+        for( var j=0 ; j< img[i].length ; j++){
+          if( i == 0 && j == 0 ) {
+            newImg[i][j] = (img[i+1][j] + img[i+1][j+1] + img[i][j+1] ) / 3;
+          }
+          else if( i == 0 && j == img[i].length  - 1 ) {
+            newImg[i][j] =( img[i+1][j] + img[i+1][j-1] + img[i][j-1] ) / 3;
+
+          }
+          else if( i == img.length - 1 && j == 0 ) {
+            newImg[i][j] = ( img[i-1][j] + img[i-1][j+1] + img[i][j+1] ) / 3;
+          }
+          else if( i == img.length - 1 && j == img[i].length  - 1 ) {
+            newImg[i][j] = ( img[i-1][j] + img[i-1][j-1] + img[i][j-1] )/ 3;
+          }  else if( i == 0 && j +1 < img[i].length  ) {
+            newImg[i][j] = ( img[i+1][j]  + img[i+1][j+1] + img[i+1][j - 1 ] + img[i][j+1] + img[i][j-1] )/ 5;
+          } else if( i == img.length -1  && j +1 < img[i].length  ) {
+            newImg[i][j] = ( img[i-1][j]  + img[i-1][j+1] + img[i-1][j - 1 ] + img[i][j+1] + img[i][j-1] )/ 5;
+          }
+          else if ( i +1 < img.length && i - 1 >= 0 && j == 0 && j+1 < img[i].length ){
+            newImg[i][j] = ( img[i+1][j] + img[i-1][j] + img[i+1][j+1] + img[i+1][j] + img[i-1][j+1] )/ 5;
+          } else if( i +1 < img.length && i - 1 >= 0 && j-1 <  img[i].length && j+1 == img[i].length  ) {
+            newImg[i][j] = ( img[i+1][j] + img[i-1][j] + img[i+1][j-1] + img[i-1][j-1] + img[i][j-1] )/ 5;
+          }
+          else {
+            newImg[i][j] = ( img[i-1][j] + img[i-1][j-1] + img[i-1][j+1] + img[i][j+1] + img[i][j-1] + img[i+1][j] +img[i+1][j+1] + img[i+1][j-1] )/ 8;
+
+          }
+
+        }
+
+      }
+
+    }
+    console.log(newImg);
+    return newImg;
+
+  }
+ // console.log("Test 1",blurImg[[5,7], [6,8]] );
 
 }
