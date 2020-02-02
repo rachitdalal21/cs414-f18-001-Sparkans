a5774b7b08203260f71261596db7bd705d500bc5
rachitdalal21 <Rachit1@3>
2020-02-02 01:18:09 -0700

    Restore All Possible IP Address from the given string of numbers.

--- Banqi-UI/src/app/home/home.component.ts
+++ Banqi-UI/src/app/home/home.component.ts
@@ -12,6 +12,7 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
 
    // this.blurImg([[1,2,3,1], [4,5, 4,1], [1,4,6,1] ]);
+    this.findAllPossibleIP("25525511135")
   }
   blurImg( img ) {
     var newImg = [];
@@ -54,6 +55,55 @@ export class HomeComponent implements OnInit {
     console.log(newImg);
     return newImg;
 
+  }
+
+  findAllPossibleIP( str ) {
+    let possibleIPCombinations = [];
+
+    if( str && ( str.length < 3 || str.length > 12 ) ) {
+      return possibleIPCombinations;
+    } else {
+      let nwStr = str,
+          strLength = str.length;
+
+      for(let i = 1; i < strLength && i < 4 ; ++i ) {
+        let first = str.substring(0,i);
+        if( !this.isValidPart( first ) ) {
+          continue;
+        }
+        for( let j = 1; i+j < strLength && j < 4; ++j ) {
+          let second = str.substring( i, i+j );
+          if( !this.isValidPart( second ) ) {
+            continue;
+          }
+
+          for( let k = 1; i+j+k < strLength && k < 4; ++k ) {
+            let third = str.substring( i+j, i+j+k );
+            let fourth = str.substring( i+j+k );
+            if( !this.isValidPart( third ) || !this.isValidPart( fourth ) ) {
+              continue;
+            }
+            let combination =  first + '.' + second + '.' + third  + '.' + fourth;
+
+            possibleIPCombinations.push(combination);
+          }
+        }
+      }
+      console.log( "possible Combination", possibleIPCombinations);
+      return possibleIPCombinations;
+    }
+  }
+
+  isValidPart( s ) {
+    if( s.length > 3 ) {
+      return false;
+    }
+    /* return false is part is start with 0 */
+    if( s.indexOf("0") == 0 && s.length > 1 ) {
+      return false;
+    }
+    let val = parseInt( s );
+      return val >= 0 && val <= 255;
   }
  // console.log("Test 1",blurImg[[5,7], [6,8]] );
 
